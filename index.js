7bc140bc41b1ca733e2475bd7852c71355fe8592
damian <damian.kieliszek@youngskiled.com>
2016-11-23 14:27:47 +0100

    Added compare method

--- index.js
+++ index.js
@@ -41,9 +41,10 @@ var versionCompare = function(left, right) {
     if (typeof left + typeof right != 'stringstring')
         return false;
     
-    var a = left.split('.')
-    ,   b = right.split('.')
-    ,   i = 0, len = Math.max(a.length, b.length);
+    var a = left.split('.'),
+      b = right.split('.'),
+      i = 0,
+      len = Math.max(a.length, b.length);
         
     for (; i < len; i++) {
         if ((a[i] && !b[i] && parseInt(a[i]) > 0) || (parseInt(a[i]) > parseInt(b[i]))) {
